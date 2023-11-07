00ce0f84b8e618e8ea183f5c9f08c41421a6a72a
dustin2050 <60738780+dustin2050@users.noreply.github.com>
2023-11-07 01:14:56 +0100

    Update SPV4.py

--- SPV4.py
+++ SPV4.py
@@ -570,237 +570,134 @@ def evaluate_model():
     plt.show()
 
 
+
 def fine_tune_model():
-    print("Finetuning the model...")
     import os
-    import signal
-
-    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
-
     import sys
     import pandas as pd
     import numpy as np
     import tensorflow as tf
-    from sklearn.preprocessing import MinMaxScaler
+    from sklearn.preprocessing import MaxAbsScaler
+    from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_percentage_error
     from tensorflow.keras.models import load_model
-    from sklearn.metrics import (
-        mean_squared_error,
-        r2_score,
-        mean_absolute_percentage_error,
-    )
-    import matplotlib.pyplot as plt
-    from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 
+    # Set the logging level to only log errors
+    os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
+    print("Finetuning the model...")
     print("TensorFlow version:", tf.__version__)
 
-    # Define custom Metric
-    def accuracy(y_true, y_pred):
-        acc = tf.reduce_mean(tf.abs((y_true / y_pred) * 100))
+    def get_reward(y_true, y_pred):
+    mape = mean_absolute_percentage_error(y_true, y_pred)
+    r2 = r2_score(y_true, y_pred)
+    reward = (((1 - mape) * 0.1) + (r2 * 1.9)) / 2
+    return reward
 
-        if tf.greater(acc, 100):
-            acc = tf.constant(0, dtype=tf.float32)
-
-        return acc
+    def create_sequences(data, timesteps):
+    X, y = [], []
+    for i in range(timesteps, len(data)):
+        X.append(data[i - timesteps: i])
+        y.append(data[i, 0])
+    return np.array(X), np.array(y)
 
     # Load data
     data = pd.read_csv("data.csv")
 
-    # Split data into train and test sets
-    train_data = data.iloc[: int(0.8 * len(data))]
-    test_data = data.iloc[int(0.8 * len(data)) :]
+    # Split data into train, validation, and test sets
+    train_size = int(0.7 * len(data))
+    validation_size = int(0.15 * len(data))
+
+    train_data = data.iloc[:train_size]
+    validation_data = data.iloc[train_size: train_size + validation_size]
+    test_data = data.iloc[train_size + validation_size:]
+
+    # Feature columns
+    feature_columns = [
+                "Close",
+                "Open",
+                "Adj Close",
+                "Volume",
+                "High",
+                "Low",
+                "SMA",
+                "MACD",
+                "upper_band",
+                "middle_band",
+                "lower_band",
+                "supertrend_signal",
+                "RSI",
+                "aroon_up",
+                "aroon_down",
+                "kicking",
+                "upper_band_supertrend",
+                "lower_band_supertrend",
+    ]
 
     # Normalize data
-    scaler = MinMaxScaler()
-    train_data_norm = scaler.fit_transform(
-        train_data[
-            [
-                "Close",
-                "Open",
-                "Adj Close",
-                "Volume",
-                "High",
-                "Low",
-                "SMA",
-                "MACD",
-                "upper_band",
-                "middle_band",
-                "lower_band",
-                "supertrend_signal",
-                "RSI",
-                "aroon_up",
-                "aroon_down",
-                "kicking",
-                "upper_band_supertrend",
-                "lower_band_supertrend",
-            ]
-        ]
-    )
-    test_data_norm = scaler.transform(
-        test_data[
-            [
-                "Close",
-                "Open",
-                "Adj Close",
-                "Volume",
-                "High",
-                "Low",
-                "SMA",
-                "MACD",
-                "upper_band",
-                "middle_band",
-                "lower_band",
-                "supertrend_signal",
-                "RSI",
-                "aroon_up",
-                "aroon_down",
-                "kicking",
-                "upper_band_supertrend",
-                "lower_band_supertrend",
-            ]
-        ]
-    )
+    scaler = MaxAbsScaler()
+    scaler.fit(train_data[feature_columns])  # Fit only to the training data
+
+    train_data_norm = scaler.transform(train_data[feature_columns])
+    validation_data_norm = scaler.transform(validation_data[feature_columns])
+    test_data_norm = scaler.transform(test_data[feature_columns])
 
     # Define time steps
     timesteps = 100
-
-    # Create sequences of timesteps
-    def create_sequences(data, timesteps):
-        X = []
-        y = []
-        for i in range(timesteps, len(data)):
-            X.append(data[i - timesteps : i])
-            y.append(data[i, 0])
-        return np.array(X), np.array(y)
-
     X_train, y_train = create_sequences(train_data_norm, timesteps)
+    X_validation, y_validation = create_sequences(validation_data_norm, timesteps)
     X_test, y_test = create_sequences(test_data_norm, timesteps)
 
-    # Load model
+    # Ask for the reward threshold
+    reward_threshold = float(input("Enter the reward threshold (0 - 1, 0.9 recommended): "))
+
+    # Initialize the best validation reward and counter for early stopping
+    best_val_reward = -np.inf
+    no_improvement_count = 0
+    early_stopping_patience = 5
+
+    # Load the model
     model = load_model("model.keras")
-    model.summary()
 
-    # Define reward threshold
-    reward_threshold = float(
-        input("Enter the reward threshold (0 - 1, 0.9 recommended): ")
-    )
+    # Training loop
+    epochs = 50
+    batch_size = 32
 
-    # Initialize rewards
-    rewards = []
-    mses = []
-    mapes = []
-    r2s = []
-    count = 0
-
-    # Function to handle SIGINT signal (CTRL + C)
-    def handle_interrupt(signal, frame):
-        print("\nInterrupt received.")
-
-        # Ask the user for confirmation
-        user_input = input(
-            f"Are you sure that you want to save the Model, Plot the Rewards and also End the Program? (yes/no): "
+    for epoch in range(epochs):
+        print(f"Epoch {epoch + 1} / {epochs}")
+        history = model.fit(
+            X_train, y_train,
+            batch_size=batch_size,
+            epochs=1,
+            validation_data=(X_validation, y_validation)
         )
 
-        if user_input.lower() == "yes":
+        # Evaluate the model on the validation set
+        val_pred = model.predict(X_validation)
+        val_reward = get_reward(y_validation, val_pred)
+
+        print(f"Validation reward for epoch {epoch + 1}: {val_reward}")
+
+        # If the current reward is better than the best one, save the model
+        if val_reward > best_val_reward:
+            print(f"Validation reward improved from {best_val_reward:.4f} to {val_reward:.4f}, saving model")
+            best_val_reward = val_reward
             model.save("model.keras")
-
-            # Plot results
-            fig, axs = plt.subplots(4, 1, figsize=(10, 10))
-            axs[0].plot(mses)
-            axs[0].set_title("MSE")
-            axs[1].plot(mapes)
-            axs[1].set_title("MAPE")
-            axs[2].plot(r2s)
-            axs[2].set_title("R2")
-            axs[3].plot(rewards)
-            axs[3].set_title("Rewards")
-            plt.tight_layout()
-            plt.show()
-
-            exit(0)
-
+            no_improvement_count = 0  # Reset counter when improvement occurs
         else:
-            # Plot results
-            fig, axs = plt.subplots(4, 1, figsize=(10, 10))
-            axs[0].plot(mses)
-            axs[0].set_title("MSE")
-            axs[1].plot(mapes)
-            axs[1].set_title("MAPE")
-            axs[2].plot(r2s)
-            axs[2].set_title("R2")
-            axs[3].plot(rewards)
-            axs[3].set_title("Rewards")
-            plt.tight_layout()
-            plt.show()
+            no_improvement_count += 1
+            print(f"No improvement in validation reward for {no_improvement_count} consecutive epoch(s).")
 
-            print("Continuing the Fine-tuning Process")
-
-    # Register the signal handler
-    signal.signal(signal.SIGINT, handle_interrupt)
-
-    while True:
-        # Load model
-        model = load_model("model.keras")
-        print("\nEvaluating Model")
-        # Evaluate model
-        y_pred = model.predict(X_test)
-        mse = mean_squared_error(y_test, y_pred)
-        mape = mean_absolute_percentage_error(y_test, y_pred)
-        r2 = r2_score(y_test, y_pred)
-
-        # Append rewards
-        reward = ((1 - mape) + r2) / 2
-        rewards.append(reward)
-        mses.append(mse)
-        mapes.append(mape)
-        r2s.append(r2)
-
-        # Print current rewards
-        print("Rewards:", rewards)
-        print("MAPE:", mape)
-        print("MSE:", mse)
-        print("R2:", r2)
-        count += 1
-        print("Looped", count, "times.")
-
-        # Check if reward threshold is reached
-        if len(rewards) >= 1 and sum(rewards[-1:]) >= reward_threshold:
-            print("Reward threshold reached!")
-            model.save("model.keras")
-
-            # Plot results
-            fig, axs = plt.subplots(4, 1, figsize=(10, 10))
-            axs[0].plot(mses)
-            axs[0].set_title("MSE")
-            axs[1].plot(mapes)
-            axs[1].set_title("MAPE")
-            axs[2].plot(r2s)
-            axs[2].set_title("R2")
-            axs[3].plot(rewards)
-            axs[3].set_title("Rewards")
-            plt.tight_layout()
-            plt.show()
-
+        # Early stopping condition
+        if no_improvement_count >= early_stopping_patience:
+            print(f"Stopping early due to no improvement in validation reward for {early_stopping_patience} consecutive epochs.")
             break
-        else:
-            # Set up callbacks
-            checkpoint = ModelCheckpoint(
-                "model.keras", save_best_only=True, verbose=1, mode="min"
-            )
-            earlystop = EarlyStopping(monitor="val_loss", patience=5, verbose=1)
 
-            # Fine-tune model)
-            print(
-                "\nReward threshold not reached, Trying to Finetune the Model with 50 Epochs. Will only save best results and will early stop after 5 non-improvements"
-            )
+        if val_reward >= reward_threshold:
+            print(f"Reward threshold of {reward_threshold} reached on validation set.")
+            break
 
-            history = model.fit(
-                X_test,
-                y_test,
-                epochs=50,
-                batch_size=32,
-                validation_data=(X_test, y_test),
-                callbacks=[checkpoint, earlystop],
-            )
+    print("Finetuning completed.")
+
+
 
 
 def predict_future_data():
